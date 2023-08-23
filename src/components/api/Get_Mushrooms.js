9f4c6bf1001e05daffdec80ff3e8b30074f3ae67
tbriggs1 <tombriggs1993@outlook.com>
2023-08-23 20:56:10 +0100

    added code for mushroom name

--- src/components/api/Get_Mushrooms.js
+++ src/components/api/Get_Mushrooms.js
@@ -3,13 +3,30 @@ import axios from 'axios'
 
 const Get_Mushrooms = () => {
 
+    const [mushrooms, setMushrooms] = useState([]); 
+
     const url = 'http://46.105.174.113:8000/mushrooms/';
+
     useEffect(() => {
         axios.get(url).then( res => {
             console.log(res.data)
-            const data = res.data;
+            setMushrooms(res.data);
         })
-    })
+        .catch(error => {
+            console.log("Error fetcing data", error)
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Mushrooms</h1>
+            <ul>
+                {mushrooms.map(mushroom => (
+                    <li key={mushroom.id}>{mushroom.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Get_Mushrooms;
